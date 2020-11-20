e989018198747784ccf2f052bb9422b9866db357
Vincent <vrdphone@gmail.com>
2020-11-19 20:26:30 -0600

    finished db.js file

--- public/db.js
+++ public/db.js
@@ -25,4 +25,35 @@ function saveRecord(record) {
     const store = transaction.objectStore("pending");
 
     store.add(record);
-}
+};
+
+function checkAllDatabase() {
+
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+    if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                // if successful, open a transaction on your pending db
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                // access your pending object store
+                const store = transaction.objectStore("pending");
+
+                // clear all items in your store
+                store.clear();
+            });
+    }
+};
